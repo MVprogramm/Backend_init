e4e647cc38b587e149eed171600925c63bd5c3db
Michael Valiahmetov <michael.valiahmetov@gmail.com>
2022-10-17 00:39:31 +0300

    add connection to mongoDB

--- index.js
+++ index.js
@@ -5,11 +5,20 @@ const bodyParser = require("body-parser")
 const messageRoutes = require("./routes/message.routes")
 
 main()
-  .then((res) => console.log('Received the following response from MongoDB: ', res))
+  .then((res) => {
+    console.log('Received the following response from MongoDB: ', res)
+
+    app.listen(8080, () => {
+      console.log("listening on port 8080")
+    })
+  })
   .catch((err) => console.log("Couldn't connect to MongoDB. Check the following error: ", err))
 
 async function main() {
-  await mongoose.connect("mongodb+srv://MICHVAL:MICHVAL16463@cluster0.tyyycpq.mongodb.net/test")
+  await mongoose.connect("mongodb+srv://MICHVAL:MICHVAL16463@cluster0.tyyycpq.mongodb.net/?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
 }
 
 app.use(bodyParser.json())
@@ -21,6 +30,3 @@ app.get("/", (req, res) => {
 
 app.use("/api/messages", messageRoutes)
 
-app.listen(8080, () => {
-  console.log("listening on port 8080")
-})
